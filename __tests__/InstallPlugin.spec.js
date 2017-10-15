718f87d78941e653cc775768eb6a0ab92c26cd33
Martin Herweg <martin@martinherweg.de>
2017-10-15 22:11:16 +0200

    write test to ensure plugin get's extracted to craftPlugin Folder

--- __tests__/InstallPlugin.spec.js
+++ __tests__/InstallPlugin.spec.js
@@ -1,19 +1,30 @@
 const InstallPlugin = require('../src/InstallPlugin');
 const fs = require('fs-extra');
+const path = require('path');
 
-const pluginUrl = 'https://github.com';
+const pluginUrl = 'https://github.com/aelvan/Imager-Craft';
 const configPath = './__mocks__/package.json';
+const mocksPath = path.resolve(__dirname, '../__mocks__/');
+
+jest.mock('./dist/craft/plugins', () => {}, { virtual: true });
+
+const mockPackage = {
+  craftPluginDownloader: {
+    plugins: [],
+    tmpFolder: path.resolve(__dirname, '../__mocks__/tmp/'),
+    pluginPath: `${path.resolve(__dirname, '../__mocks__/dist/craft/plugins/')}/`,
+  },
+};
 
 afterAll(() => {
-  fs.writeJSONSync('./__mocks__/package.json', {
-    craftPluginDownloader: {
-      plugins: [],
-      pluginPath: './dist/craft/plugins/',
-    },
-  });
+  fs.emptyDirSync(`${mocksPath}/tmp/`);
+  fs.emptyDirSync(`${mocksPath}/dist/craft/plugins/`);
 });
 
 beforeAll(async () => {
+  fs.emptyDirSync(`${mocksPath}/tmp/`);
+  fs.emptyDirSync(`${mocksPath}/dist/craft/plugins/`);
+  fs.outputJsonSync(`${mocksPath}/package.json`, mockPackage);
   await InstallPlugin({
     config: './__mocks__/package.json',
     pluginUrl,
@@ -25,4 +36,6 @@ test('it adds the Plugin Url to an craftPlugins array in package.json', async ()
   expect(pkg.craftPluginDownloader.plugins).toContain(pluginUrl);
 });
 
-test('It extracts the plugin in the defined Plugin Folder', () => {});
+test('It extracts the plugin in the defined Plugin Folder', () => {
+  expect(fs.pathExistsSync(`${mocksPath}/dist/craft/plugins/imager`)).toBeTruthy();
+});
